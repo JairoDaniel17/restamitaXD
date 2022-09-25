fb4875e24974eace10dc23c7340da3ca6665eaf2
Jairo Mendoza <jairodaniel.mt@gmail.com>
2022-09-25 15:30:09 -0500

    Commit
    Signed-off-by: Jairo Mendoza <jairodaniel.mt@gmail.com>

---
+++ src/main/java/xd/restamita/RestamitaApplication.java
@@ -0,0 +1,13 @@
+package xd.restamita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestamitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestamitaApplication.class, args);
+    }
+
+}
